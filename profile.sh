711baadbba178022554f948980b10a98a978c5a9
MarinBozinovic <marinbozinovic125@gmail.com>
2024-09-02 15:48:40 +0200

    test

--- profile.sh
+++ profile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir ~/.aws
+mkdir -p ~/.aws
 
-cat >> ~/.aws/credentials << EOF
+cat > ~/.aws/credentials << EOF
 [default]
 aws_access_key_id = $AWS_S3_ACCESS_KEY
 aws_secret_access_key = $AWS_S3_SECRET
 EOF
 
-cat >> ~/.aws/config << EOF 
+cat > ~/.aws/config << EOF 
 [default]
 region = $AWS_REGION
 EOF
